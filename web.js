e6b1166f874f5c01e1187fc50de384037c766608
Vladimir Petrigo <taenaru@gmail.com>
2013-12-19 13:48:53 +0000

    Corrected a stupid mistake

--- web.js
+++ web.js
@@ -7,7 +7,7 @@ app.set('views', path.join(__dirname, 'views'));
 app.use(express.logger());
 
 app.get('/', function(request, response) {
-    var buf = fs.readFileSync('views' + 'index.html');
+    var buf = fs.readFileSync('views' + '/index.html');
     var string = buf.toString('utf8', 0, buf);
     response.send(string);
 });
