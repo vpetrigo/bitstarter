79a446e1b1bc86e1590bbcf3f092ad37fb3a8629
Vladimir Petrigo <taenaru@gmail.com>
2013-12-19 13:45:31 +0000

    Corrected a stupid mistake

--- web.js
+++ web.js
@@ -8,7 +8,7 @@ app.use(express.logger());
 
 app.get('/', function(request, response) {
     var buf = fs.readFileSync('views' + 'index.html');
-    var string = buf.toString('utf-8', 0, buf.lenght);
+    var string = buf.toString('utf8', 0, buf);
     response.send(string);
 });
 
